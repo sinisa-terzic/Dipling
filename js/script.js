2427061f9b0c7542f87cfb1fe5c0b48d91bdda3d
sinisa-terzic <sinisat.ita21@gmail.com>
2023-05-16 15:53:59 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+///////////////////////////////////////////////////////////
+// oprn dialog box library
+/* const library = document.querySelector('.library');
+
+const library_Close = document.querySelector('.library-close')
+library_Close.addEventListener('click', () => {
+    library.classList.add("none");
+    // library_Open.classList.add('open-library-is-open');
+});
+
+const library_Open = document.querySelector('.open-library')
+library_Open.addEventListener('click', () => {
+    library.classList.remove("none");
+    // library_Open.classList.remove('open-library-is-open');
+});
+ */
