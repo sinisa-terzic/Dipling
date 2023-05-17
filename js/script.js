d2283ee49c918b50890cd1dda0575d7dd94c010b
sinisa-terzic <sinisat.ita21@gmail.com>
2023-05-17 13:18:19 +0200

    work on library

--- js/script.js
+++ js/script.js
@@ -4,14 +4,39 @@ const library = document.querySelector('.library');
 
 const library_Close = document.querySelector('.library-header-close')
 library_Close.addEventListener('click', () => {
-    library.classList.add("none");
-    // library_Open.classList.add('open-library-is-open');
+    library.classList.add("translateX-100");
 });
 
 const library_Open = document.querySelector('.open-library')
 library_Open.addEventListener('click', () => {
-    library.classList.remove("none");
-    library.classList.add("grid");
-    // library_Open.classList.remove('open-library-is-open');
+    library.classList.remove("translateX-100");
+});
+
+// oprn dialog box library
+const library_Pdf1 = document.querySelector('.library-pdf1');
+
+const library_Pdf_Close1 = document.querySelector('.library-pdf1')
+library_Pdf_Close1.addEventListener('click', () => {
+    library_Pdf1.classList.add("none");
+    console.log("klik")
+});
+
+const library_Pdf_Open1 = document.querySelector('.link-pdf1')
+library_Pdf_Open1.addEventListener('click', () => {
+    library_Pdf1.classList.remove("none");
+});
+
+// 2
+const library_Pdf2 = document.querySelector('.library-pdf2');
+
+const library_Pdf_Close2 = document.querySelector('.library-pdf2')
+library_Pdf_Close2.addEventListener('click', () => {
+    library_Pdf2.classList.add("none");
+    console.log("klik")
+});
+
+const library_Pdf_Open2 = document.querySelector('.link-pdf2')
+library_Pdf_Open2.addEventListener('click', () => {
+    library_Pdf2.classList.remove("none");
 });
 
