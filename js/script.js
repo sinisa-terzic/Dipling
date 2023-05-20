e185b1a46b25d5e62afd1ac962c57d95bbbf47d5
sinisa-terzic <sinisat.ita21@gmail.com>
2023-05-20 13:18:10 +0200

    add loader

--- js/script.js
+++ js/script.js
@@ -60,3 +60,25 @@ function myFunction() {
     }
 }// JavaScript Document
 
+
+///////////////////////////////////////////////////////////
+// remove loader
+const body = document.querySelector("body"),
+    loader = document.querySelector(".loader-wrap")
+
+function fadeOutEffect() {
+    const fadeEffect = setInterval(function () {
+        if (!loader.style.opacity) {
+            loader.style.opacity = 1;
+        }
+        if (loader.style.opacity > 0) {
+            loader.style.opacity -= 0.4;
+        } else {
+            body.classList.remove('stop-scroll');
+            loader.classList.add('remove');
+            clearInterval(fadeEffect);
+        }
+    }, 100);
+}
+window.addEventListener("load", fadeOutEffect);
+
